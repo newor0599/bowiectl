9ba4f5ccdc6a6dc55bda72198c23e15c689ca569
newor0599 <rowen778623@gmail.com>
2025-10-26 12:30:34 +0800

    Added battery GET

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from bleak import BleakClient, BleakScanner
 from bleak.backends.device import BLEDevice
 
 # UUID
-# Read : 654b749c-e37f-ae1f-ebab-40ca133e3690
-# Write: ee684b1a-1e9b-ed3e-ee55-f894667e92ac
+# Read  : 654b749c-e37f-ae1f-ebab-40ca133e3690
+# Write : ee684b1a-1e9b-ed3e-ee55-f894667e92ac
 
 
 class BASEUS:
@@ -57,14 +57,21 @@ class APP:
             print("[INFO] Quitting!")
             return
         print("[INFO] Earbud found")
+        await self.base.client.start_notify(
+            "654b749c-e37f-ae1f-ebab-40ca133e3690", self.notif_hander
+        )
+        print("[INFO] Setup notification handler")
         while True:
-            out = await asyncio.to_thread(input, f"[{self.base.address}] ")
+            out = input(f"[{self.base.address}] ")
             out = out.split(" ")
             match out[0]:
                 case "anc":
                     await self.anc(out[1])
-                case "quit":
+                case "quit" | "exit":
                     break
+                case "battery":
+                    await self.base.write("ba02")  # Left Right Battery
+                    await self.base.write("ba27")  # Case Battery
                 case "help":
                     print("anc {value:int|str}")
                     print(" Sets ANC profile")
@@ -79,8 +86,20 @@ class APP:
                     print(" quit program")
                     print("help")
                     print(" print help")
+                case _:
+                    print("Invalid command")
         print("[INFO] Cleaning up")
         await self.base.client.disconnect()
+
+    async def notif_hander(self, sender, data):
+        if data[0] == 170 and data[1] == 2:
+            print(f"Left earbud  : {data[2]}")
+            print(f"Right earbud : {data[4]}")
+            return
+        if data[0] == 170 and data[1] == 39:
+            print(f"Case         : {data[2]}")
+            return
+        print(f"[NOTIF] {sender} -> {data.hex()}\n")
 
     async def anc(self, value: str):
         presets = {
@@ -107,4 +126,5 @@ class APP:
         await self.base.write(code)
 
 
-asyncio.run(APP().main())
+if __name__ == "__main__":
+    asyncio.run(APP().main())
